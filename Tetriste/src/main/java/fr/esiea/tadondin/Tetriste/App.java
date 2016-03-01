cf44712b18f6476b0b68a2f98782ad0868df0762
ta <ta@et.esiea.fr>
2016-03-01 16:18:57 +0100

    Importation du projet Maven sur github

---
+++ Tetriste/src/main/java/fr/esiea/tadondin/Tetriste/App.java
@@ -0,0 +1,13 @@
+package fr.esiea.tadondin.Tetriste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
