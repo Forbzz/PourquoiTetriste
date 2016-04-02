372d251a47b31f13da68b90f03e9484b86b2f1cc
Forbzz <ta@et.esiea.fr>
2016-04-02 20:39:03 +0200

    Game.java added (beginning)

---
+++ Tetriste/src/main/java/fr/esiea/tadondin/Tetriste/Game.java
@@ -0,0 +1,72 @@
+package fr.esiea.tadondin.Tetriste;
+
+public class Game {
+
+	private boolean gameEnded=false;
+	
+public Game(Coordinate C){
+
+	
+
+	
+}
+
+protected Gameboard getGameboard(){
+	
+	
+}
+
+public void moveCurrentPieceLeft(){
+	
+	
+	
+}
+
+
+public void moveCurrentPieceRight(){
+	
+	
+}
+
+public void moveCurrentPieceDown(){
+	
+	
+}
+
+public void rotateCurrentPieceCounterclockwise(){
+	
+	
+}
+
+public void rotateCurrentPieceClockwise(){
+	
+	
+}
+
+public boolean nextPiece(){
+	
+	
+	return nextPiece() ;
+}
+
+public String toString(){
+	
+	return toString();
+}
+
+public boolean isCurrentPieceFixed(){
+	
+	return isCurrentPieceFixed();
+}
+
+public boolean isGameEnded(){
+	
+	return isGameEnded();
+}
+
+
+
+
+
+
+}
